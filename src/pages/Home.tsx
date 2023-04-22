3c95302a1d201277d0e4096bbfdd06d97c877d2a
Monica Marybel Avila Alcazar <road_line@hotmail.com>
2023-04-21 21:19:12 -0500

    feat: click event on home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,47 @@
+import { Chip, Container } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { Container } from '@mui/material';
+
+const emojis = ['✨', '🎉', '👋🏼', '🔥', '💥', '🦄', '🌈', '🤠', '🦁', '🐯'];
 
 export default function Home() {
-  const [colorTitle, setColorTitle] = useState('000000');
+  const [location, setLocation] = useState({ top: 0, left: 0 });
+  const [emoji, setEmoji] = useState(emojis[0]);
+  const [opacity, setOpacity] = useState(0);
   useEffect(() => {
-    const handleMouseMove = (event: MouseEvent) => {
-      setColorTitle(event.clientX.toString(16));
+    const handleMouseClick = (event: MouseEvent) => {
+      const { clientX, clientY } = event;
+      setLocation({ top: clientY, left: clientX });
+      setEmoji(emojis[Math.floor(Math.random() * emojis.length)]);
+      setOpacity(1);
+      const timer = setTimeout(() => {
+        setOpacity(0);
+        clearTimeout(timer);
+      }, 1000);
     };
 
-    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('click', handleMouseClick);
 
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('click', handleMouseClick);
     };
   }, []);
 
   return (
     <Container>
-      <h1 style={{ transition: 'color 1s', color: `#${colorTitle}` }}>
-        Welcome to my personal site.
-      </h1>
+      <h1>Welcome to my personal site.</h1>
+      <Chip variant="outlined" label={`Click on the page ${emoji}`} />
+      <span
+        style={{
+          transition: 'opacity 1s',
+          opacity,
+          position: 'absolute',
+          top: `${location.top}px`,
+          left: `${location.left}px`,
+          fontSize: '3rem',
+        }}
+      >
+        {emoji}
+      </span>
     </Container>
   );
 }
